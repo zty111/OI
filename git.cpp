01dedc13ed6dc4743e3679512eacbee6fc435103
zty111 <1225013181@qq.com>
2019-10-23 21:40:13 +0800

    2019

--- git.cpp
+++ git.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 using namespace std;
 int main() {
-    system("git add *");
+    system("git add .");
     system("git commit -m \"2019\"");
     system("git push -u origin master");
     printf("OK\n");
