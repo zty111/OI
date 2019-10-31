5f49047ef3f8d17eb1677ad38f1829fd44e85abc
zty111 <1225013181@qq.com>
2019-10-31 23:18:02 +0800

    2019

--- 2.cpp
+++ 2.cpp
@@ -15,48 +15,32 @@ inline int read(){
     return x * f;
 }
 const int N = 1000010;
-int c[N], nxt[N], h[N], to[N], cnt, dis[N], r[N], d[N], siz[N];
+int c[N], nxt[N], h[N], to[N], cnt, r[N], d[N], siz[N];
 void add_edge(int u, int v) {
     cnt++; nxt[cnt] = h[u]; to[cnt] = v; h[u] = cnt;
 }
+bool cmp(int a, int b) {
+    return d[a] + siz[b] * 2 > d[b] + siz[a] * 2;
+}
+int tim, tot;
 void dfs(int u, int fa) {
-    siz[u] = 1;
-    d[u] = c[u] + dis[u];
+    if(u != 1) d[u] = c[u];
     for(int i = h[u]; i; i = nxt[i]) {
         int v = to[i];
         if(v == fa) continue;
-        dis[v] = dis[u] + 1;
         dfs(v, u);
-        siz[u] += siz[v];
     }
-}
-bool cmp(int a, int b) {
-    return d[a] + (siz[b] - 1) * 2 > d[b] + (siz[a] - 1) * 2;
-}
-int tim, tot;
-void dfs2(int u, int fa) {
-    ans = max(ans, c[u] + tim);
-    int now = tot;
-    for(int i = h[u]; i; i = nxt[i]) {
+    tot = 0;
+    for(int i = h[u]; i; i = nxt[i]){
         int v = to[i];
         if(v == fa) continue;
         r[++tot] = v;
     }
-    int now2 = tot;
-    sort(r+now+1, r+1+tot, cmp);
-    for(int i = now + 1; i <= now2; i++) {
+    sort(r+1, r+1+tot, cmp);
+    for(int i = 1; i <= tot; i++) {
         int v = r[i];
-        tim++;
-        dfs2(v, u);
-    }
-    tim++;
-}
-void dp(int u, int fa) {
-    for(int i = h[u]; i; i = nxt[i]) {
-        int v = to[i];
-        if(v == fa) continue;
-        dp(v, u);
-        d[u] = max(d[v], d[u]);
+        d[u] = max(d[u], d[v] + siz[u] * 2 + 1);
+        siz[u] += siz[v] + 1;
     }
 }
 int main() {
@@ -67,10 +51,8 @@ int main() {
         add_edge(a, b);
         add_edge(b, a);
     }
-    dis[1] = 1; ans = c[1] + 2 * (n - 1);
+    ans = c[1] + 2 * (n - 1);
     dfs(1, 0);
-    dp(1, 0);
-    dfs2(1, 0);
-    printf("%d\n", ans);
+    printf("%d\n", max(d[1], ans));
     return 0;
 }
